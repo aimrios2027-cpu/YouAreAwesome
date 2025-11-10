ab76665ae3f1af45882416dd2a69eebf91c24942
Aimee Rios <aimrios2027@gmail.com>
2025-11-10 11:44:40 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by RIOS, AIMEE on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width:200, height:200)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+    
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great"){
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
