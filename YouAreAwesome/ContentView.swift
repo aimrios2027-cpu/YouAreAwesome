72f891767df53606d2510ded38d8192db6559e2f
Aimee Rios <aimrios2027@gmail.com>
2025-11-21 11:46:25 -0800

    Save changes

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,51 +16,46 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius:30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
            
             
             Button("Show Message") {
                 let messages = ["You are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You Are Great!",
                                 "You Are Fantastic!",
                                 "Fabulous? That's You!",
-                                "You Make Me Smile",
-                                "When the Genius Bar Needs Help, They Call You!"]
+                                "You Make Me Smile"]
                 
-                message = messages[messageNumber]
-                messageNumber = messageNumber + 1
-                if messageNumber == message.count {
-                    messageNumber = 0
-                }
+                message = messages[Int.random(in: 0...messages.count-1)]
                 
-                imageName = "image\(imageNumber)"
-                imageNumber += 1
-                
-                if imageNumber > 9 {
-                    imageNumber = 0
+                imageName = "image\(Int.random(in: 0...9))"
                 }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-        
+            .padding()
         }
-        .padding()
+     
         
     }
-}
 
 #Preview {
     ContentView()
